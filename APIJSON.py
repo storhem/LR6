dbca8dbf6752288ff8c9c32d229486d003215e48
storhem <storhetmax@mail.ru>
2025-12-12 22:53:12 +0300

    added the POST method

--- APIJSON.py
+++ APIJSON.py
@@ -14,3 +14,16 @@ def read_item(item_id: int):
     if item_id not in items_db:
         raise HTTPException(status_code=404, detail="Item not found")
     return items_db[item_id]
+
+@app.post("/items")
+def create_item(item_id: int, name: str, description: Optional[str] = None):
+    if item_id in items_db:
+        raise HTTPException(status_code=400, detail="Item already exists")
+
+    item = {
+        "id": item_id,
+        "name": name,
+        "description": description
+    }
+    items_db[item_id] = item
+    return item
